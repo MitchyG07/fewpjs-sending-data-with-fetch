c1ff7009158d7ca72b27a15df7be240c8da26aa0
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-14 12:20:31 -0800

    commit tree 661bd9802854372f7a7eb34a17e172d7a5b8a2a0

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// }
+
+// let configurationObject = {
+//     method: "POST",
+//     headers: { 
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)        
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(object){
+//         console.log(object);
+//     })
+//     .catch(function(error){
+//         alert("Bad things! Ragnarők!")
+//         console.log(error.message)
+//     })
+
+let body = document.querySelector('body')
+let div = document.createElement('div')
+
+function submitData() {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+   return fetch("http://localhost:3000/users",configObj)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(object){
+            div.textContent = object.id
+            body.appendChild(div)
+        })
+        .catch(function(error){
+            alert("come one brotha don't break it")
+            console.log(error.message)
+            div.textContent = error.message
+            body.appendChild(div)
+        })
+}
